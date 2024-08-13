4aa72e80df613d553edef811e42215ac4a9b41db
adarsh kumar <adarshupadhyay231501@gmail.com>
2024-08-13 21:30:46 +0530

    added responsive behaviour to both file

--- src/utils/Productcarts.jsx
+++ src/utils/Productcarts.jsx
@@ -3,16 +3,15 @@ import { Link, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import Productcall from '../Store/actions/productaction';
 import Header from './Header';
-import axios from 'axios';
 import { productInsert } from '../Store/reducers/productreducer';
 import allcategcall from '../Store/actions/categoriecallaction';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import { Inserttocart } from '../Store/reducers/cartreducer';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { customToastOptions } from './Header';
-
+import addtocart from '../Store/actions/cartaction';
+import { useAddToCartBackend } from './Addtocartbackend';
+import axios from 'axios';
+import '../styles/Productcart.css'
 function Productcarts() {
   const params=useParams()
   const [subcateg,setsubcateg]=useState([]);
@@ -58,6 +57,17 @@ getsubcateg()
 
 
 
+//custom hook 
+const{addtocartbackend,runner}=useAddToCartBackend()
+const value=useSelector((state)=>state.Cart.value)
+
+
+//once runnner change means new items added to backend to cart so add new cart collection to redux cart
+useEffect(()=>{
+  des(addtocart())
+  
+  console.log(value)},[runner])
+
 
 useEffect(()=>{
 
@@ -90,41 +100,43 @@ setcategorychoosen(e.target.dataset.subcateg)
 
 }
 
-const addtocart=async(ele)=>{
-  const ele2={...ele,choosenquantity:1}
-  des(Inserttocart(ele2))
-  try{ const {data:{message}}=await
-  axios.post('http://localhost:3000/api/v1/Inserttocart',{id:ele2._id,choosenquantity:ele2.choosenquantity},{withCredentials:true})
-  console.log(message)
-  toast.success(message,customToastOptions)
-}
-catch(err){
-  console.log(err.message)
-}
+const[size,setsize]=useState(window.innerWidth)
+
+useEffect(()=>{
+window.addEventListener('resize',()=>{
+
+  const sizewidth=window.innerWidth
+  setsize(sizewidth)
+})
 
 
+},[])
 
 
-  }
 
   return (   
    <div className='overflow-y-hidden overflow-x-hidden h-screen w-screen'>
   <Header />
-     <div className='flex  overflow-y-hidden'>
-       <div className='w-[20%] pt-3'>
-         <div className='text-[2.2vw] mb-[2rem] font-medium'>Category</div>
-         <span className='text-[1.8vw] ml-[1rem]'>{params.category}</span>
+     <div className='flex relative overflow-y-hidden'>
+      <button onClick={()=>{setsize(501)}} className='absolute top-1 left-1 hover:scale-105'> <i className={`ri-menu-line  text-[1.4rem] text-red-600 ${size>500 && "hidden"} `}></i></button>
+    
+       <div className={`w-[20%] h-screen sidepanel pt-3  ${size<501&&"ml-[-15rem]"}`}>
+       <button   onClick={()=>{setsize(500)}}  className={`ml-[90%]  ${size>501 &&"hidden" }    mt-[2px]`}><i class="ri-close-line text-red-500 "></i></button>
+         <div className='lg:text-[2.2vw]  md:text-[2.2vw] text-[1.3rem] mb-[2rem] font-medium'>Category </div>
+         <span className='lg:text-[1.8vw] md:text-[1.9vw] text-[1rem] ml-[1rem]'>{params.category}</span>
          <div className='flex ml-[2rem] gap-[1rem] flex-col'>
            {subcateg.map((ele, index) => (
-             <div key={index}  onClick={getfilterproduct}    data-subcateg={ele.name} className=' hover:cursor-pointer  flex text-[1.5vw]'>
+             <div key={index}  onClick={getfilterproduct}   
+              data-subcateg={ele.name} className=' hover:cursor-pointer  flex text-[0.7rem]   md:text-[1.6rem] lg:text-[1.5vw]'>
                <i className='ri-user-smile-fill'></i>{ele.name}
              </div>
            ))}
-<div className='text-[1.3vw]'>Price Range:${range[0]}-${range[1]}</div>
-           <Slider   range      value={range} min={76}  max={13000} onChange={(value)=>{setrange(value)}}/>
+<div className='lg:text-[1.3vw]  md:text-[1.3vw] text-[2.6vw]'>Price Range:${range[0]}-${range[1]}</div>
+<div className='lg:w-[100%] md:w-[100%] w-[35vw] text-red-500 pr-3' ><Slider   range      value={range} min={76}  max={13000} onChange={(value)=>{setrange(value)}}/></div>
+           
          </div>
        </div>
-       <div className='flex w-[80%] h-[88vh] overflow-y-scroll flex-wrap items-center justify-center gap-[3rem]'>
+       <div className='flex w-[80%]  productright    h-[88vh] overflow-y-scroll flex-wrap items-center justify-center gap-[3rem]'>
          {categproduct.map((ele, index) => (
         <div  className='mt-[2rem] flex flex-col gap-2  items-center rounded-lg hover:scale-105 shadow-lg  w-[30vw]   md:w-[24vw]    lg:w-[15vw] min-h-[25vw]'        key={index} >
           <Link    to={`/products/${params.category}/${ele.slug}`}    className=' flex items-center justify-center text-center  flex-col '>
@@ -132,7 +144,7 @@ catch(err){
              <span className='text-[3vw] md:text-[2vw] lg:text-[1.5vw] mx-2 my-1 text-zinc-700'>{ele.name.slice(0,35)}...</span>
               <span className=' text-[2.4vw]  md:text-[1.7vw]  lg:text-[1.1vw]'>${ele.price}</span>
               
-            </Link><button   onClick={()=>{addtocart(ele)}}        className='bg-yellow-400 mb-2 rounded-2xl lg:w-[8vw] w-[13vw]  text-[2.3vw]  md:text-[1.6vw]   lg:text-[1.2vw] py-1'>Add to cart</button>
+            </Link><button   onClick={()=>{addtocartbackend(ele)}}        className='bg-yellow-400 mb-2 rounded-2xl lg:w-[8vw] w-[13vw]  text-[2.3vw]  md:text-[1.6vw]   lg:text-[1.2vw] py-1'>Add to cart</button>
             </div>  
           ))}
         </div> 
--- src/utils/Searchproduct.jsx
+++ src/utils/Searchproduct.jsx
@@ -51,21 +51,41 @@ des(Inserttocart(ele2))
 toast.success("Added to cart",customToastOptions)
 }
 
-  return (<div className='overflow-x-hidden'> <Header/>
-    <div className='flex'>
 
-    <div className='w-[30%] pt-3'>
+
+const[size,setsize]=useState(window.innerWidth)
+
+useEffect(()=>{
+window.addEventListener('resize',()=>{
+
+  const sizewidth=window.innerWidth
+  setsize(sizewidth)
+})
+
+
+},[])
+
+
+
+  return (<div className='overflow-x-hidden h-screen w-screen overflow-y-hidden'> <Header/>
+    <div className='flex relative'>
+    <button onClick={()=>{setsize(501)}} className='absolute top-1 left-1 hover:scale-105'> <i className={`ri-menu-line  text-[1.4rem] text-red-600 ${size>500 && "hidden"} `}></i></button>
+    <div className={`w-[30%] sidepanel h-screen ${size<501&&"ml-[-15rem]"}  pt-3`}>
+         
+    <button   onClick={()=>{setsize(500)}}  className={`ml-[90%]  ${size>501 &&"hidden" }    mt-[2px]`}><i class="ri-close-line text-red-500 "></i></button>
          <div className='text-[2.2vw] mb-[2rem] font-medium'>Price Range</div>
          <span className='text-[1.8vw] ml-[1rem]'></span>
          <div className='flex ml-[2rem] gap-[1rem] flex-col'>
-            <div className='text-[1.4vw] font-semibold'>Price limit:${pricerange[0]}-${pricerange[1]}</div>
+            <div className='lg:text-[1.3vw]  md:text-[1.3vw] text-[2.6vw]'>Price limit:${pricerange[0]}-${pricerange[1]}</div>
           
-         <Slider value={pricerange}  className='w-[70%] text-blue-700' range min={100} max={13000} onChange={(value)=>{setpricerange(value)}}/>
+         
+            <div className='lg:w-[100%] md:w-[100%] w-[35vw] text-red-500' ><Slider value={pricerange}  className='w-[70%] text-blue-700' range min={100} max={13000} onChange={(value)=>{setpricerange(value)}}/></div>
+         
          
          </div>
        </div>
 
-<div className='flex flex-col'>
+<div className='flex flex-col w-[70%] h-[88vh] overflow-y-auto  productright'>
 {keyproduct.map((ele, index) => (
     <div className='rounded-lg flex flex-col items-center  shadow-lg mt-[1rem]  min-h-[25vw]'  key={index} >
       <Link       to={`/products/search/${keyword}/${ele.slug}`}          className='flex  items-center gap-[3rem]  w-[60vw]' key={index}>
