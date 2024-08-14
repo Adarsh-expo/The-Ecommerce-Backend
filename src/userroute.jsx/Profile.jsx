945871a06fcb2f3aa115b7f8f746b33f5e91606a
adarsh kumar <adarshupadhyay231501@gmail.com>
2024-08-14 22:44:36 +0530

    error corrected

--- src/userroute.jsx/Profile.jsx
+++ src/userroute.jsx/Profile.jsx
@@ -3,13 +3,30 @@ import Usermenu from '../utils/Usermenu'
 import Header from '../utils/Header'
 function Profile() {
     document.title="Your Profile"
+
+
+
+    const[size,setsize]=useState(window.innerWidth)
+
+    useEffect(()=>{
+    window.addEventListener('resize',()=>{
+    
+      const sizewidth=window.innerWidth
+      setsize(sizewidth)
+    })
+    
+    
+    },[])
+
+
+
   return (<div> 
     
     <Header/>
     
      <div className='flex '>
     
-    <Usermenu/>
+    <Usermenu  size={size} setsize={setsize}/>
   <div>profile</div>
 
 </div> </div>
