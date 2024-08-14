20fd91e06792f5e70bcd2956d8fd8d1689a1ce3b
adarsh kumar <adarshupadhyay231501@gmail.com>
2024-08-14 23:05:59 +0530

    scroll added

--- src/userroute.jsx/Orders.jsx
+++ src/userroute.jsx/Orders.jsx
@@ -13,7 +13,7 @@ function Orders() {
 useEffect(()=>{
 const getdetail=async()=>{
 
-  const res= await axios.get('https://ecommerce-backend-teif.onrender.com/api/v1/detail/getorderdetail',{withCredentials:true})
+  const res= await axios.get('http://localhost:3000/api/v1/detail/getorderdetail',{withCredentials:true})
 console.log(res.data)
 setorder(res.data.Order)
 }
@@ -37,28 +37,30 @@ window.addEventListener('resize',()=>{
 
 
 
-  return (<div className='   items-center  w-[100vw] h-[100vh] overflow-x-hidden ' >
+  return (<div className='   items-center  w-[100vw] h-[100vh] overflow-y-hidden overflow-x-hidden ' >
     
     
 
     <Header  />
-    <div className='flex relative '>
+    <div className='flex relative  '>
       
     <button onClick={()=>{setsize(501)}} className='absolute top-1 left-1 hover:scale-105'> <i className={`ri-menu-line  text-[1.4rem] text-red-600 ${size>500 && "hidden"} `}></i></button>
     <Usermenu size={size} setsize={setsize} />
-  <div className='  flex flex-col gap-[2rem] productright2   w-[80vw]'>
+  <div className='  flex flex-col gap-[2rem] productright2  mt-2 h-[80vh]  overflow-y-auto  w-[80vw]'>
 
-{order.length ? order.map((ele,index)=><Orderboxcart  key={index}  wholedata={ele} />):<div className='text-[1.5vw]'>No order history to show</div>}
+{order.length ? order.map((ele,index)=><Orderboxcart  key={index}  wholedata={ele} />):<div className=' mt-4  ml-8  lg:text-[1.5vw]
+ md:text-[2vw] text-[4vw]'>No order history to show</div>}
 
 
 
 
   </div>
+  <Footer/>
 
 </div>
 
-<Footer/>
 
+<Footer/>
  </div>
     
   )
