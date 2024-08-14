6bce8976a264889082edb4be2ff802cb74343b46
adarsh kumar <adarshupadhyay231501@gmail.com>
2024-08-14 22:28:45 +0530

    shipping made responsive

--- src/components/Shippingaddress.jsx
+++ src/components/Shippingaddress.jsx
@@ -65,8 +65,8 @@ console.log(user)
 
         console.log(addressobject)
 
-        const {data}=  await axios.post('https://ecommerce-backend-teif.onrender.com/api/v1/gateway/ordercheckout',{sumtotal})
-        const{data:{key}}=await axios.get('https://ecommerce-backend-teif.onrender.com/api/v1/gateway/apikey')
+        const {data}=  await axios.post('http://localhost:3000/api/v1/gateway/ordercheckout',{sumtotal})
+        const{data:{key}}=await axios.get('http://localhost:3000/api/v1/gateway/apikey')
         
         var options = {
           key, 
@@ -80,7 +80,7 @@ console.log(user)
 
 handler:function(res){
 
-axios.post('https://ecommerce-backend-teif.onrender.com/api/v1/gateway/paymentverfication',{addressobject,res,amount:data.data.amount*1/100,selectedproduct,id:user._id})
+axios.post('http://localhost:3000/api/v1/gateway/paymentverfication',{addressobject,res,amount:data.data.amount*1/100,selectedproduct,id:user._id})
 .then((res)=>{console.log(res.data.message)
 if(res.data.orderid){window.location.href=`/Paymentsuccessfull/?reference_id=${res.data.orderid}`}
 
@@ -133,75 +133,185 @@ catch((err)=>{
 
 
   return (
-    <div className='w-[100vw] flex flex-col items-center h-[100vh]'>
-<div className='w-[95vw] border-b-[1px] border-b-zinc-400 flex justify-center items-center text-[2.7vw]  h-[8vw] bg-zinc-200'>
-     <div className=''>Checkout  </div>
+    <div className="w-full flex flex-col items-center h-screen">
+  {/* Header */}
+  <div className="w-[95vw] border-b border-b-zinc-400 flex justify-center items-center text-[4vw] sm:text-[2.7vw] h-[12vw] sm:h-[8vw] bg-zinc-200">
+    <div>Checkout</div>
+  </div>
 
-</div>
-{/* div containg both form and payment option */}
-<div className=' w-[100%]  flex  h-[84vh]'>
-    {/* section of address form */}
-<div className='w-[70%] addresection overflow-y-auto flex flex-col'>
-<span className='text-orange-500 ml-[5vw]  text-[2vw] font-medium '>Enter The shipping address</span>
-<form  noValidate onSubmit={checkvalidity}   className='flex mb-[2rem] border-zinc-300 rounded-lg border-[1px] px-[2rem]  ml-[7vw] mt-[1rem]  w-[75%] flex-col'>
-<span className='text-[2vw] font-semibold' >Add a new address</span>
-<label className='text-[1.3vw] text-zinc-800   font-semibold'>Country/Region</label>
-<select  value={addressobject.Country}  name='Country' onChange={addressformhandler} className=' border-[1px] h-[2.6rem] text-[1.4vw] pl-[0.7rem]   outline-offset-4 outline-[2rem]    outline-[#037386] rounded border-black'>
-{countries? countries.map((ele)=><option>{ele.name}</option>):<option>None</option>}
-    
-</select>
+  {/* Main content area */}
+  <div className="w-full flex flex-col md:flex-row h-full">
+    {/* Address form section */}
+    <div className="w-full md:w-2/3 overflow-y-auto flex flex-col p-4">
+      <span className="text-orange-500 ml-[5vw] text-[4vw] sm:text-[2vw] font-medium">
+        Enter The Shipping Address
+      </span>
+      <form
+        noValidate
+        onSubmit={checkvalidity}
+        className="flex mb-8 border border-zinc-300 rounded-lg p-4 md:ml-[7vw] mt-4 w-full md:w-[75%] flex-col"
+      >
+        <span className="text-[4vw] sm:text-[2vw] font-semibold">
+          Add a new address
+        </span>
+        <label className="text-[3vw] sm:text-[1.3vw] text-zinc-800 font-semibold mt-4">
+          Country/Region
+        </label>
+        <select
+          value={addressobject.Country}
+          name="Country"
+          onChange={addressformhandler}
+          className="border h-[3rem] text-[3vw] sm:text-[1.4vw] p-2 outline-none rounded border-black"
+        >
+          {countries
+            ? countries.map((ele) => <option>{ele.name}</option>)
+            : <option>None</option>}
+        </select>
 
-<label     className='text-[1.3vw] text-zinc-800    mt-[1rem] font-semibold'>State</label>
-<input  required   value={addressobject.State}   placeholder='State' name='State' onChange={addressformhandler}    className='  bg-white border-[1px] h-[2.6rem]  text-[1.4vw] pl-[0.7rem]  rounded outline-yellow-700 border-black' type='text'/>
-<label     className='text-[1.3vw] text-zinc-800    mt-[1rem] font-semibold'>Full name</label>
-<input  required  value={addressobject.fullname}   placeholder='Ener Full Name' name='fullname' onChange={addressformhandler}    className=' bg-white    text-[1.4vw] pl-[0.7rem] border-[1px] h-[2.6rem]     outline-yellow-700 rounded border-black' type='text'/>
-<label                         className='text-[1.3vw] text-zinc-800  font-semibold    mt-[1rem]'  >City</label>
-<input     required   value={addressobject.city}   placeholder='Enter city'     name='city'  onChange={addressformhandler}      className=' bg-white   text-[1.4vw] pl-[0.7rem] border-[1px] h-[2.6rem]     outline-yellow-700 rounded border-black'              text='text'/>
-<label className='text-[1.3vw] text-zinc-800  font-semibold    mt-[1rem]'    >Flat no</label>
-<input     required   value={addressobject.Flatno}   placeholder=' Flat No /House No'    name='Flatno'  onChange={addressformhandler}         className='  bg-white  border-[1px] h-[2.6rem] text-[1.4vw] pl-[0.7rem]    outline-yellow-700 rounded border-black' type='text'/>
-<label className='text-[1.3vw] text-zinc-800  font-semibold  mt-[1rem]'        >Street no</label>
-<input   required     value={addressobject.streetno}  placeholder='Street No'  name='streetno'   onChange={addressformhandler}        className='bg-white  border-[1px] h-[2.6rem] text-[1.4vw] pl-[0.7rem]    outline-yellow-700 rounded border-black'    type='text'/>
-<label className='text-[1.3vw] text-zinc-800  font-semibold    mt-[1rem]'   >Landmark</label>
-<input  required  value={addressobject.Landmark}   placeholder='Mention Landmark'   name='Landmark'    onChange={addressformhandler}      className='bg-white  border-[1px] h-[2.6rem] text-[1.4vw] pl-[0.7rem]    outline-yellow-700 rounded border-black'    type='text'/>
+        <label className="text-[3vw] sm:text-[1.3vw] text-zinc-800 font-semibold mt-4">
+          State
+        </label>
+        <input
+          required
+          value={addressobject.State}
+          placeholder="State"
+          name="State"
+          onChange={addressformhandler}
+          className="bg-white border h-[3rem] text-[3vw] sm:text-[1.4vw] p-2 rounded outline-none border-black"
+          type="text"
+        />
 
-<label className='text-[1.3vw] text-zinc-800  font-semibold    mt-[1rem]'  >Phone No</label>
-<input    required  value={addressobject.phoneno}  placeholder='Enter Phone no'   name='phoneno'  onChange={addressformhandler}        className=' bg-white border-[1px] h-[2.6rem] text-[1.4vw] pl-[0.7rem]    outline-yellow-700 rounded border-black' type='tel'/>
-<label className='text-[1.3vw] text-zinc-800  font-semibold    mt-[1rem]'    >Zipcode</label>
-<input value={addressobject.Zipcode}   placeholder='Zip code'       name='Zipcode' onChange={addressformhandler}      className='bg-white  border-[1px] h-[2.6rem] text-[1.4vw] pl-[0.7rem]   outline-yellow-700 rounded border-black'    type='text' maxLength={6} inputMode='numeric'  pattern="[0-9]{6}"   required />
+        <label className="text-[3vw] sm:text-[1.3vw] text-zinc-800 font-semibold mt-4">
+          Full name
+        </label>
+        <input
+          required
+          value={addressobject.fullname}
+          placeholder="Enter Full Name"
+          name="fullname"
+          onChange={addressformhandler}
+          className="bg-white text-[3vw] sm:text-[1.4vw] p-2 border h-[3rem] rounded outline-none border-black"
+          type="text"
+        />
 
+        <label className="text-[3vw] sm:text-[1.3vw] text-zinc-800 font-semibold mt-4">
+          City
+        </label>
+        <input
+          required
+          value={addressobject.city}
+          placeholder="Enter City"
+          name="city"
+          onChange={addressformhandler}
+          className="bg-white text-[3vw] sm:text-[1.4vw] p-2 border h-[3rem] rounded outline-none border-black"
+          type="text"
+        />
 
+        <label className="text-[3vw] sm:text-[1.3vw] text-zinc-800 font-semibold mt-4">
+          Flat no
+        </label>
+        <input
+          required
+          value={addressobject.Flatno}
+          placeholder="Flat No /House No"
+          name="Flatno"
+          onChange={addressformhandler}
+          className="bg-white text-[3vw] sm:text-[1.4vw] p-2 border h-[3rem] rounded outline-none border-black"
+          type="text"
+        />
 
+        <label className="text-[3vw] sm:text-[1.3vw] text-zinc-800 font-semibold mt-4">
+          Street no
+        </label>
+        <input
+          required
+          value={addressobject.streetno}
+          placeholder="Street No"
+          name="streetno"
+          onChange={addressformhandler}
+          className="bg-white text-[3vw] sm:text-[1.4vw] p-2 border h-[3rem] rounded outline-none border-black"
+          type="text"
+        />
 
-<button    className='bg-yellow-400 mb-[2rem] mt-[1rem] rounded text-[1.5vw] font-light h-[2.5rem]'  >Use this address</button>
+        <label className="text-[3vw] sm:text-[1.3vw] text-zinc-800 font-semibold mt-4">
+          Landmark
+        </label>
+        <input
+          required
+          value={addressobject.Landmark}
+          placeholder="Mention Landmark"
+          name="Landmark"
+          onChange={addressformhandler}
+          className="bg-white text-[3vw] sm:text-[1.4vw] p-2 border h-[3rem] rounded outline-none border-black"
+          type="text"
+        />
 
-</form>
+        <label className="text-[3vw] sm:text-[1.3vw] text-zinc-800 font-semibold mt-4">
+          Phone No
+        </label>
+        <input
+          required
+          value={addressobject.phoneno}
+          placeholder="Enter Phone no"
+          name="phoneno"
+          onChange={addressformhandler}
+          className="bg-white text-[3vw] sm:text-[1.4vw] p-2 border h-[3rem] rounded outline-none border-black"
+          type="tel"
+        />
 
-</div>
+        <label className="text-[3vw] sm:text-[1.3vw] text-zinc-800 font-semibold mt-4">
+          Zipcode
+        </label>
+        <input
+          value={addressobject.Zipcode}
+          placeholder="Zip code"
+          name="Zipcode"
+          onChange={addressformhandler}
+          className="bg-white text-[3vw] sm:text-[1.4vw] p-2 border h-[3rem] rounded outline-none border-black"
+          type="text"
+          maxLength={6}
+          inputMode="numeric"
+          pattern="[0-9]{6}"
+          required
+        />
 
-{/* //order summary */}
-<div className='smallbox w-[30%]  pl-[3vw] h-[30vw]'>
-
-<div className='border-[1px] flex  px-3  flex-col border-black mt-4 rounded-lg w-[75%] h-[80%]'>
-
-<span  className='text-[1vw]'>Choose a shipping address and payment method to calculate.</span>
-<hr/>
-<div className='  text-[1.3vw]  font-semibold mt-[1rem]'>Order Summary</div>
-<div className='text-[1vw] mt-2'>Total Items:{subtotal}</div>
-<hr className='my-3'/>
-<div className='text-[1.4vw]   font-semibold flex  justify-between  text-red-800'><div>OrderTotal:</div><div className='items-center flex '><i class=" text-[2vw] ri-money-rupee-circle-fill"></i>
-<NumericFormat value={sumtotal} displayType='text'   thousandSeparator/></div> </div>
-<button  disabled={!addressready}     onClick={()=>{checkoutorder(sumtotal)}} className={`${!addressready?"bg-yellow-200":"bg-yellow-400"}   mb-[10%] h-[2rem] flex justify-center items-center rounded-lg mt-[5%]`}>Pay to Order</button>
-
-</div>
-
-</div>
-
-</div>
-
-
-
-
+        <button className="bg-yellow-400 mt-4 rounded text-[4vw] sm:text-[1.5vw] font-light h-[3rem]">
+          Use this address
+        </button>
+      </form>
     </div>
+
+    {/* Order summary */}
+    <div className="w-full md:w-1/3 p-4 flex flex-col items-center">
+      <div className="border flex flex-col border-black rounded-lg w-full h-auto p-4 bg-white">
+        <span className="text-[4vw] sm:text-[1vw]">
+          Choose a shipping address and payment method to calculate.
+        </span>
+        <hr className="my-4"/>
+        <div className="text-[4vw] sm:text-[1.3vw] font-semibold">
+          Order Summary
+        </div>
+        <div className="text-[4vw] sm:text-[1vw] mt-2">Total Items: {subtotal}</div>
+        <hr className="my-4"/>
+        <div className="text-[4vw] sm:text-[1.4vw] font-semibold flex justify-between text-red-800">
+          <div>Order Total:</div>
+          <div className="flex items-center">
+            <i className="text-[6vw] sm:text-[2vw] ri-money-rupee-circle-fill"></i>
+            <NumericFormat value={sumtotal} displayType="text" thousandSeparator />
+          </div>
+        </div>
+        <button
+          disabled={!addressready}
+          onClick={() => { checkoutorder(sumtotal); }}
+          className={`${!addressready ? "bg-yellow-200" : "bg-yellow-400"} mb-4 h-[3rem] flex justify-center items-center rounded-lg mt-4`}
+        >
+          Pay to Order
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
   )
 }
 
