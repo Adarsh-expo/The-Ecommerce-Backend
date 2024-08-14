545914edae482c0c7c9980ae34e9e5fe89176be7
adarsh kumar <adarshupadhyay231501@gmail.com>
2024-08-14 22:27:11 +0530

    order page made responsive

--- src/userroute.jsx/Orders.jsx
+++ src/userroute.jsx/Orders.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 import { useEffect,useState } from 'react'
 import Header from '../utils/Header'
 import Footer from '../utils/Footer'
+import '../styles/Order.css'
 function Orders() {
     document.title="Your Orders"
     const[order,setorder]=useState([])
@@ -12,7 +13,7 @@ function Orders() {
 useEffect(()=>{
 const getdetail=async()=>{
 
-  const res= await axios.get('https://ecommerce-backend-teif.onrender.com/api/v1/detail/getorderdetail',{withCredentials:true})
+  const res= await axios.get('http://localhost:3000/api/v1/detail/getorderdetail',{withCredentials:true})
 console.log(res.data)
 setorder(res.data.Order)
 }
@@ -22,15 +23,30 @@ getdetail()
 
 
 
+const[size,setsize]=useState(window.innerWidth)
 
-  return (<div className='  items-center  w-[100vw] h-[100vh] overflow-x-hidden ' >
+useEffect(()=>{
+window.addEventListener('resize',()=>{
+
+  const sizewidth=window.innerWidth
+  setsize(sizewidth)
+})
+
+
+},[])
+
+
+
+  return (<div className='   items-center  w-[100vw] h-[100vh] overflow-x-hidden ' >
     
-    <Header/>
-    <div className='flex '>
+    
+
+    <Header  />
+    <div className='flex relative '>
       
-    
-    <Usermenu/>
-  <div className='  flex flex-col gap-[2rem]    w-[80vw]'>
+    <button onClick={()=>{setsize(501)}} className='absolute top-1 left-1 hover:scale-105'> <i className={`ri-menu-line  text-[1.4rem] text-red-600 ${size>500 && "hidden"} `}></i></button>
+    <Usermenu size={size} setsize={setsize} />
+  <div className='  flex flex-col gap-[2rem] productright2   w-[80vw]'>
 
 {order.length ? order.map((ele,index)=><Orderboxcart  key={index}  wholedata={ele} />):<div className='text-[1.5vw]'>No order history to show</div>}
 
