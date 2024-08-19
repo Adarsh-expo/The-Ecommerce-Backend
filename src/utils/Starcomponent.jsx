9c3db37cb3853802e6c362d023acf2b9d623ac40
adarsh kumar <adarshupadhyay231501@gmail.com>
2024-08-19 17:01:21 +0530

    slight change made

--- src/utils/Starcomponent.jsx
+++ src/utils/Starcomponent.jsx
@@ -3,24 +3,27 @@ import React from 'react'
 function Starcomponent({star}) {
 
   return (
-    <div className='flex text-orange-300 lg:text-[2vw]'>
+    <div className='flex text-orange-500 lg:text-[2vw]'>
   { Array.from({length:5},(ele,index)=>{
     
-    let number=index+0.5;
-    let number2=index+1;
+  
+    let number=index+1;
     
-    if(number===star){
+    if(number>star && number-1>=star ){
 
 
 
 
-    return (<div><i class="ri-star-half-line"></i> </div>);
+    return (<div><i class="ri-star-line"></i> </div>);
+   
 
-}
-    if(number>star){
-       return (<div><i class="ri-star-line"></i></div>)
+} if(number<star){
+      return (<div><i class="ri-star-fill"></i></div>)
     }
-    if(number<star)
+    if(number>star && number-1<star){
+       return (<div><i class="ri-star-half-line"></i></div>)
+    }
+    if(number===star)
     {
         return(<div><i class="ri-star-fill"></i></div>)
     }})
